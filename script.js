5e2ab05a4cfffb4a6afc0d40b429db320cab5f8a
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-05-08 21:12:56 +0300

    Append input to container as child

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ window.onload = () => {
       input.type = "text";
       input.id = letter + number;
       input.ariaLabel = letter + number;
+      container.appendChild(input);
     });
   });
 };
