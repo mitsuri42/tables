f6941215da7f80ff0b2e6a029a5bd1a7bb431130
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-05-09 22:17:26 +0300

    Add median function

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const isEven = (num) => num % 2 === 0;
 const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
 const average = (nums) => sum(nums) / nums.length;
 
+const median = (nums) => {
+  const sorted = nums.slice().sort((a, b) => a - b);
+};
+
 const range = (start, end) =>
   Array(end - start + 1)
     .fill(start)
